9ffe40015f07c9b40694b89539599282d05e801d
Emily Deans <emilyldeans@gmail.com>
2017-07-27 16:04:40 -0400

    Created loginRoutes.js and installed morgan npm.

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const app = express();
 const mustacheExpress = require("mustache-express");
+const bodyParser = require("body-parser");
 const session = require("express-session");
+const expressValidator = require("express-validator");
+const fs = require("fs");
+const morgan = require("morgan");
 
 const usersPermit = {
   email: "cookiesareyummy@homework.com",
   password: "nOmN0m503"
 };
 
+// sets up body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.engine("mustache", mustacheExpress());
 
+// turn on template engine
 app.set("view engine", "mustache");
+
+// assign where views are stored
+app.set("views", __dirname + "/views");
+
+// sets up express-validator
+app.use(expressValidator());
+
+
+app.get("/", (req, res) => {
+  res.render("index", {});
+  console.log("Can I read mustache?");
+});
 
 // this sets up a session store using the express-session npm
 app.use(session({
     secret: 'what-is-the-secret?',
     resave: false,
     saveUninitialized: false,
-    cookie: {secure: true }
+    // cookie: {secure: true }
   })
 );
 
