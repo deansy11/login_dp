6a365617649e9b096c9bcc9277b0febedd87a61b
Emily Deans <emilyldeans@gmail.com>
2017-08-20 18:12:08 -0400

    Corrected cookie middleware.

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const app = express();
 const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-const expressValidator = require("express-validator");
-const fs = require("fs");
-const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const login = require("./loginRoutes");
+const url = require("url");
 
 const userPermit = {
   name: "Wonder Woman",
@@ -15,30 +13,41 @@ const userPermit = {
   password: "password"
 };
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", __dirname + "/views");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "helloPass",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.views) {
+    req.session.views = 0;
+  }
+  req.session.views += 1;
+  next();
+})
+
+app.use((req, res, next) => {
+  if(req.body.email === userPermit.email) {
+    if (req.body.password === userPermit.password) {
+      req.session.login = true;
+      req.session.name = userPermit.name;
+    }
+  }
+  next();
 });
 
-app.use(cookieParser);
-app.use(session);
-app.get("/user/:user", (req, res) => {
-  res.cookie("name", req.params.user)
-  console.log(req.params.user);
-})
+app.use(require("./loginRoutes"))
 
-app.get("/user", (req, res) => {
-  res.send(req.cookies.name);
-})
-app.use(expressValidator());
-app.use(morgan('dev'));
-
-
-app.listen(3000, () =>{
+app.listen(3000, () => {
   console.log("Node running at http://localhost:3000");
 });
--- loginRoutes.js
+++ loginRoutes.js
@@ -1,125 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const cookieParser = require("cookie-parser");
-router.use(cookieParser);
 
+router.get("/", (req, res) => {
+  console.log("Hello?");
+  if (req.session.login === true) {
+    res.redirect("/loggedIn");
+  } else {
+    res.render("index", {});
+  }
+});
 
-// const sessionStore = {
-// };
-//
-// const user =  {
-//     "id" : "id",
-//     "email" : "cookiesareyummy@homework.com",
-//     "password" : "nOmN0m503"
-// };
+router.get("/loggedIn", (req, res) => {
+  if (req.session.login === true) {
+    res.render("loginSuccess", { user: req.session})
+  } else {
+    res.redirect("/")
+  }
+});
 
-// function isLoggedIn(req) {
-//   return req.session.name !== undefined
-// };
+router.post("/login", (req, res) => {
+  if (req.session.login === true) {
+    res.redirect("/userLogin")
+  } else {
+    res.redirect("/");
+  }
+});
 
-router.get("/user/:user", (req, res) => {
-  res.cookie("name", req.params.user)
-  res.send(`<p>Cookie Set: <a href="/user">View Here</a>`)
-  console.log(req.params.user);
-})
-
-router.get("/user", (req, res) => {
-  res.send(req.cookies.name);
-})
-
-// router.get("/", (req, res) => {
-//   if(sessionStore[req.session.id]) {
-//     res.send("You're logged in!");
-//   } else {
-//     res.send("Please login.");
-//   }
-// });
-//
-// router.get("/login", (req,res) => {
-//   if(isLoggedIn(req)) {
-//     res.redirect("/")
-//   } else {
-//     res.render("login");
-//   }
-// });
-// router.post("/userLogin", (req, res) => {
-//   if(req.body.email === user.email && req.body.password === user.password) {
-//     req.body.user = user.email;
-//     req.body.password = user.password;
-//     sessionStore[req.session.id] = req.session;
-//     res.render("index", { loggedIn: "You're logged in!"});
-//   } else {
-//     res.render("index", { error: "You're not logged in. Please try again."});
-//     return;
-//   }
-// });
-//
-// module.exports = router;
-//
-
-
-
-
-
-
-
-
-
-
-
-
-// router.get("/", (req, res) => {
-//   console.log("Is working?");
-//   res.render("index", {
-//     user : user
-//   });
-// });
-
-//
-// router.post("/userLogin"), (req, res) => {
-//   if(req.body)
-// }
-
-//
-// router.post("/userLogin", (req, res) => {
-//   console.log("Post request", req.body);
-//   req.checkBody("email", "You must provide an email.").notEmpty();
-//
-//   req.getValidationResult().then(result => {
-//      if (!result.isEmpty()) {
-//        res.render("index", { error: "Please enter your login information."});
-//        return;
-//      }
-//      user.push({
-//        email: req.user.email,
-//        password: req.user.password
-//      });
-//      res.redirect("/loggedIn");
-//    });
-// });
-
-
-// router.post("/userLogin", (req, res) => {
-//   console.log("Login request", req.body);
-//   req.checkBody("email", "You must provide an email.").notEmpty();
-//
-//   req.getValidationResult().then(result => {
-//     if (!result.isEmpty()) {
-//       res.render("index", { error: "Please enter your login information."});
-//     }
-//
-//   })
-// })
-//
-//
-//
-// router.post("/logins", (req, res) => {
-//   console.log("Is this working??", req.body);
-//   req.checkBody("email", "You must provide an email.").notEmpty();
-//
-//   req.getValidationResult().then(result => {
-//     if (!result.isEmpty()) {
-//       res.render("index", { error: "Please enter your email." });
-//       return;
-//     }
-// });
+module.exports = router;
